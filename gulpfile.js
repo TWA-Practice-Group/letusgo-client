5ab31780c49fc75f6382c2c570f424f22a7615cb
fgcui <fgcui1204@163.com>
2015-03-02 17:40:33 +0800

    init project

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+
+// var jade = require('gulp-jade');
+
+var $ = require('gulp-load-plugins')();
+
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('less_compiler', function () {
+
+    return  gulp.src('app/styles/*.less')
+        .pipe($.less())
+        .pipe(gulp.dest('.tmp/styles'));
+});
+
+gulp.task('react_compiler', ['less_compiler'], function() {
+
+    browserify(['./app/scripts/main.js'])
+        .transform(reactify)
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('./.tmp/scripts'));
+});
+
+gulp.task('jade_compiler', ['react_compiler'], function(){
+
+    return gulp.src('./app/*.jade')
+        .pipe($.jade())
+        .pipe(gulp.dest('./.tmp/'))
+});
+
+gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
+
+gulp.task('serve', ['jade_compiler'], function () {
+    browserSync({
+        notify: false,
+        port: 9000,
+        server: {
+            baseDir: ['.tmp', 'app'],
+            routes: {
+                '/bower_components': 'bower_components'
+            }
+        }
+    });
+
+    gulp.watch([
+        'app/*.html',
+        'app/scripts/*.js',
+        'app/styles/*.less',
+        'app/*.jade'
+    ]).on('change', reload);
+
+    gulp.watch('app/styles/*.less', ['less_compiler']);
+    gulp.watch('app/scripts/*.js',['react_compiler']);
+    gulp.watch('app/*.jade', ['jade_compiler'])
+});
+
+
+gulp.task('default', ['clean'], function () {
+    gulp.start('build');
+});
